97f1c65c93178392894c9cfdc683f8f79a7fd3da
darshan-debug <darshanks2012@gmail.com>
2025-08-11 23:57:20 +0530

    first commit

---
+++ src/main/java/com/example/springWebServer/SpringWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springWebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebServerApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springWebServer/SpringWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springWebServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
